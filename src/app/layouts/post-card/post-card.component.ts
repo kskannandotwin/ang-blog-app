670eb8401c4bca03e2cbfced354a06f162871b5e
kannanATexcelforte <kannan@excelforte.com>
2023-08-11 12:04:35 +0530

    11.08.2023-spdtpcc
    completed

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-card',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-card.component.css']
 })
 export class PostCardComponent {
+
+  @Input() postData: Object | any;
+
+  ngOnInit() {
+    console.log(this.postData);
+  }
 
 }
